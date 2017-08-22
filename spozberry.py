c9d4f12dc8c015cda5da7dd9d2d1e4d29eb56466
Hugo Kawamata <hugokawamata@gmail.com>
2017-08-23 01:04:21 +1000

    Add basic draft of functionality in comments

--- spozberry.py
+++ spozberry.py
@@ -14,13 +14,27 @@ if __name__ == '__main__':
   client.timeout = 10
   client.connect("localhost", 6600)
 
+  # Make a list of all the playlists with $ at the start
+
+  # Randomly order them
+
+
   while True:
+    phoneIsConnected = False
     time.sleep(5)
     # TODO: remove sudo from the following command:
     p = subprocess.Popen("sudo arp-scan -l | grep " + phoneMAC, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
     if output:
+      print(client.listplaylists())
       print("It's on the network!")
+      if !phoneIsConnected: # The phone just connected
+        phoneIsConnected = True
+        client.load() # Load the first playlist in the queue
+      else:
+        # Check if playlist is almost run out
+        # Randomly pick another playlist with a different name
+        # Load that playlist
     else:
       print("It's not on the network")
