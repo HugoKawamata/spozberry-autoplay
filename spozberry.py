13d83009246e0aa8714ae60f86c4646d9ee2476f
Hugo Kawamata <hugokawamata@gmail.com>
2017-08-23 15:45:23 +1000

    Make album skipping awesome, and a whole bunch of other stuff

--- spozberry.py
+++ spozberry.py
@@ -6,38 +6,77 @@ from threading import Thread
 
 # My phone's mac address = 9c:b2:b2:90:ef:1e
 
+def randomise_by_album(client):
+  currentPlaylist = client.playlistinfo()
+  shuffledPlaylist = []
+  i = 0
+  #while client.playlistinfo()[i]["album"]
+  while i < len(currentPlaylist):
+    album = []
+    album.append(currentPlaylist[i])
+    i += 1
+    while i < len(currentPlaylist) and currentPlaylist[i]["album"] == album[0]["album"]:
+      album.append(currentPlaylist[i])
+      i += 1
+
+    shuffledPlaylist.append(album)
+
+  client.clear()
+  random.shuffle(shuffledPlaylist)
+  for album in shuffledPlaylist:
+    for song in album:
+      client.add(song["file"])
+  
+
+
 def wait_for_input(L, client):
   paused = False
 
   while(1):
     print("listening...")
-    print("L is " + str(L))
     L.append(input())
 
     if len(L) > 0:
       if " " in L[0]: # Reload Playlist
         client.clear()
-        play_random_playlist(playlistList, currentPlaylist)
-      if "n" in L[0]: # N for next
-        client.next()
-      if "p" in L[0]: # P for pause/play
-        if paused == True:
-          client.play()
-        else:
-          client.pause()
+        play_random_playlist(playlistList)
+      else:
+        if "n" in L[0]: # N for next
+          client.next()
+        if "p" in L[0]: # P for pause/play
+          if paused == True:
+            client.play()
+          else:
+            client.pause()
+        if "a" in L[0]: # A for skip to next album
+          currentAlbum = client.currentsong()["album"]
+          print(currentAlbum)
+          i = int(client.status()["song"])
+          while i < len(client.playlistinfo()): 
+            if client.playlistinfo()[i]["album"] == currentAlbum:
+              i += 1
+            else:
+              break
+
+          if i < len(client.playlistinfo()):
+            client.play(i)
+          else:
+            client.stop()
 
       while L != []: # Make sure L is always empty at the start of a listen
         L.remove(L[0])
 
 
-def play_random_playlist(playlistList, lastPlaylistName = None):
-  playlistName = lastPlaylistName
-  while playlistName == lastPlaylistName:
-    playlistNum = random.randint(0, len(playlistList) - 1)
-    playlistName = playlistList[playlistNum]["playlist"]
+def play_random_playlist(playlistList):
+  playlistNum = random.randint(0, len(playlistList) - 1)
+  playlistName = playlistList[playlistNum]["playlist"]
   print("Playing playlist " + playlistName)
   client.clear()
-  client.load(playlistName) # Load a random playlist
+  if playlistName[1] == "@":
+    client.load(playlistName)
+    randomise_by_album(client) # Shuffle but keep albums together
+  else:
+    client.load(playlistName) # Load a random playlist
   client.play(0)
   return playlistName
 
@@ -55,9 +94,7 @@ if __name__ == '__main__':
   client.connect("localhost", 6600)
 
   unfilteredPlaylists = client.listplaylists()
-  playlistList = list(filter(lambda plDict: plDict["playlist"][0] == "$", unfilteredPlaylists))
-
-  currentPlaylist = ""
+  playlistList = list(filter(lambda plDict: plDict["playlist"][1] == "@", unfilteredPlaylists))
 
   L = []
   inputThread = Thread(target = wait_for_input, args = (L, client))
@@ -77,11 +114,11 @@ if __name__ == '__main__':
 
       if phoneIsConnected == False: # The phone just connected
         phoneIsConnected = True
-        currentPlaylist = play_random_playlist(playlistList)
+        play_random_playlist(playlistList)
       else:
-        if len(client.playlistinfo()) == 0:
-          print(client.playlist())
-          play_random_playlist(playlistList, currentPlaylist)
+        print(client.status()["state"])
+        if client.status()["state"] == "stop":
+          play_random_playlist(playlistList)
 
 
     else:
