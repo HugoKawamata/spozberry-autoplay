8ae7f3243c239b0f1993299b3997256d42eb7ee2
Hugo Kawamata <hugokawamata@gmail.com>
2017-08-23 12:40:11 +1000

    Add commands

--- spozberry.py
+++ spozberry.py
@@ -6,6 +6,30 @@ from threading import Thread
 
 # My phone's mac address = 9c:b2:b2:90:ef:1e
 
+def wait_for_input(L, client):
+  paused = False
+
+  while(1):
+    print("listening...")
+    print("L is " + str(L))
+    L.append(input())
+
+    if len(L) > 0:
+      if " " in L[0]: # Reload Playlist
+        client.clear()
+        play_random_playlist(playlistList, currentPlaylist)
+      if "n" in L[0]: # N for next
+        client.next()
+      if "p" in L[0]: # P for pause/play
+        if paused == True:
+          client.play()
+        else:
+          client.pause()
+
+      while L != []: # Make sure L is always empty at the start of a listen
+        L.remove(L[0])
+
+
 def play_random_playlist(playlistList, lastPlaylistName = None):
   playlistName = lastPlaylistName
   while playlistName == lastPlaylistName:
@@ -26,13 +50,17 @@ if __name__ == '__main__':
   phoneMAC = MyMAC
 
   client = MPDClient()
-  client.timeout = 10
+  client.timeout = 20
   client.connect("localhost", 6600)
 
   unfilteredPlaylists = client.listplaylists()
   playlistList = list(filter(lambda plDict: plDict["playlist"][0] == "$", unfilteredPlaylists))
 
   currentPlaylist = ""
+
+  L = []
+  inputThread = Thread(target = wait_for_input, args = (L, client))
+  inputThread.start()
 
   while True:
     if playlistList == []:
@@ -41,7 +69,8 @@ if __name__ == '__main__':
     p = subprocess.Popen("sudo arp-scan -l | grep " + phoneMAC, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
-    if output:
+    
+    if output: # If the phone is connected
       failedTicks = 0
       print("It's on the network!")
 
@@ -53,12 +82,10 @@ if __name__ == '__main__':
           print(client.playlist())
           play_random_playlist(playlistList, currentPlaylist)
 
-        # Check if playlist is almost run out
-        # Randomly pick another playlist with a different name
-        # Load that playlist
+
     else:
       print("It's not on the network")
       failedTicks += 1
-      if failedTicks > 4:
+      if failedTicks > 6:
         client.clear()
         phoneIsConnected = False
